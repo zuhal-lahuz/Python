0f95e6db1a0ee7f0b93bcceb7510ba042ceef3a3
zuhal-lahuz <zuhalawencon@gmail.com>
2025-02-19 21:52:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ a = 10
 b = 2
 print ("hasil penjumlahan a dan b adalah", a+b)
 print ("zuhal coba push")
+print ("namanya juga coba")
 
 print(time.time() - Start_time, "detik")
 #kalo mau compile
