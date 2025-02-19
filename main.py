f5afe984b2c0eca6efdc4500dc82990811e47e8e
zuhal <zuhalawencon@gmail.com>
2025-02-19 23:17:50 +0700

    main.py

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ print ("zuhal")
 print(time.time() - Start_time, "detik")
 #kalo mau compile
 #python -m py_compile main.py                    
+
+
+# Data Type int,float,complex,bool,str
+data_int = 9
+print("data :", data_int, ", type :", type(data_int))
+
+data_float = float(data_int)
+print("data :", data_float, ", type :", type(data_float))
+
+data_str = str(data_int)
+print("data :", data_str, ", type :", type(data_str))   
+
+data_bool = bool(data_int) 
+print("data :", data_bool, ", type :", type(data_bool))
