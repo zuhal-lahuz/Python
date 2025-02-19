ae27d846c0db897ee2ecf59847afc02fb284c411
zuhal <zuhalawencon@gmail.com>
2025-02-19 21:51:12 +0700

    main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ adalah komentar multi baris """
 a = 10
 b = 2
 print ("hasil penjumlahan a dan b adalah", a+b)
+print ("zuhal coba push")
 
 print(time.time() - Start_time, "detik")
 #kalo mau compile
