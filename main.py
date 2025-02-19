54a948290748cef901b534cdf8a1d9533987853c
zuhal <zuhalawencon@gmail.com>
2025-02-19 21:44:53 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#comment zuhal
+import time
+
+Start_time = time.time()
+print ("Zuhal")
+""" ini 
+adalah komentar multi baris """
+
+a = 10
+b = 2
+print ("hasil penjumlahan a dan b adalah", a+b)
+
+print(time.time() - Start_time, "detik")
+#kalo mau compile
+#python -m py_compile main.py                    
