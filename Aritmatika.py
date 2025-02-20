b12e65ba425a1d28e906ebed2989fb8c5653c994
zuhal <zuhalawencon@gmail.com>
2025-02-20 17:49:46 +0700

    coba aritmatika suhu

---
+++ Aritmatika.py
@@ -0,0 +1,22 @@
+#20 Februari 2025
+
+print("Suhu Dalam Berbagai Satuan")
+celcius = float(input("Masukkan Suhu dalam Celcius: "))
+
+print("Suhu dalam Celcius: ", celcius, "\u00B0C")
+
+# Reamur
+reamur = (4/5) * celcius
+print("Suhu dalam Reamur: ", reamur, "\u00B0C")
+
+# Fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("Suhu dalam Fahrenheit: ", fahrenheit, "\u00B0C")
+
+# Kelvin
+kelvin = celcius + 273
+print("Suhu dalam Kelvin: ", kelvin, "\u00B0C")
+
+
+
+
